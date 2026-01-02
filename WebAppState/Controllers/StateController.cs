d2c557a0930740c36f745a7255b75665f6d4142b
Mohit verma <mohitverma9817578959@gmail.com>
2026-01-02 11:27:10 +0530

    Updates

--- WebAppState/Controllers/StateController.cs
+++ WebAppState/Controllers/StateController.cs
@@ -6,14 +6,78 @@ namespace WebAppState.Controllers
 {
     public class StateController : Controller
     {
-       private readonly StateRepo _stateRepo;
+        private readonly StateRepo _stateRepo;
         public StateController(StateRepo stateRepo)
         {
             _stateRepo = stateRepo;
         }
         public IActionResult Index()
         {
+            var states = _stateRepo.GetStatesAsync();
+            return View(states);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var state = _stateRepo.GetStateByIdAsync(id);
+            if (state == null)
+            {
+                return NotFound();
+            }
+            return View(state);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _stateRepo.DeleteStateAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Create()
+        {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(State state)
+        {
+            if (ModelState.IsValid)
+            {
+                _stateRepo.CreateStateAsync(state);
+                return RedirectToAction("Index");
+            }
+            return View(state);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var state = _stateRepo.GetStateByIdAsync(id);
+            if (state == null)
+            {
+                return NotFound();
+            }
+            return View(state);
+        }   
+        [HttpPost]
+        public IActionResult Edit(State state)
+        {
+            if (ModelState.IsValid)
+            {
+                _stateRepo.UpdateStateAsync(state);
+                return RedirectToAction("Index");
+            }
+            return View(state);
+        }
+
+        public IActionResult GetByName(string name)
+        {
+            var state = _stateRepo.GetStateByNameAsync(name);
+            if (state == null)
+            {
+                return NotFound();
+            }
+            return View("Details", state);
+        }
+
     }
 }
---
+++ WebAppState/Migrations/20260102052916_Initails.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAppState.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Districts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Districts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "States",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_States", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cities");
+
+            migrationBuilder.DropTable(
+                name: "Districts");
+
+            migrationBuilder.DropTable(
+                name: "States");
+        }
+    }
+}
