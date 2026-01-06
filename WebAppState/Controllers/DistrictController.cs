c9a965f6eaec850b1efd4d7d80cb18472f8dcd1e
Mohit verma <mohitverma9817578959@gmail.com>
2026-01-06 13:04:24 +0530

    updates

--- WebAppState/Controllers/DistrictController.cs
+++ WebAppState/Controllers/DistrictController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net.NetworkInformation;
+using WebAppState.Models;
+using WebAppState.Repos.Interfaces;
 
 namespace WebAppState.Controllers
 {
     public class DistrictController : Controller
     {
-        public IActionResult Index()
+        private readonly IStates<District> _district;
+   
+
+        public DistrictController(IStates<District> states)
         {
-            return View();
+            _district = states;
+          
+        }
+        public async Task<IActionResult> Index()
+        {
+          var result = await _district.GetAllAsync();
+
+
+            return View(result);
         }
     }
 }
